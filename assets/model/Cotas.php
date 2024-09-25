2728dbd43522f7ad6d93af045b8fbd6edfb719c6
JP1005YT <godlolpro32@gmail.com>
2024-09-25 13:35:34 -0300

    cotas model

---
+++ assets/model/Cotas.php
@@ -0,0 +1,67 @@
+<?php
+class Cota {
+    private $id;
+    private $produtoId;
+    private $fornecedorId;
+    private $precoUnitario;
+    private $quantidade;
+    private $dataCotacao;
+
+    public function __construct($id, $produtoId, $fornecedorId, $precoUnitario, $quantidade, $dataCotacao) {
+        $this->id = $id;
+        $this->produtoId = $produtoId;
+        $this->fornecedorId = $fornecedorId;
+        $this->precoUnitario = $precoUnitario;
+        $this->quantidade = $quantidade;
+        $this->dataCotacao = $dataCotacao;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getProdutoId() {
+        return $this->produtoId;
+    }
+
+    public function setProdutoId($produtoId) {
+        $this->produtoId = $produtoId;
+    }
+
+    public function getFornecedorId() {
+        return $this->fornecedorId;
+    }
+
+    public function setFornecedorId($fornecedorId) {
+        $this->fornecedorId = $fornecedorId;
+    }
+
+    public function getPrecoUnitario() {
+        return $this->precoUnitario;
+    }
+
+    public function setPrecoUnitario($precoUnitario) {
+        $this->precoUnitario = $precoUnitario;
+    }
+
+    public function getQuantidade() {
+        return $this->quantidade;
+    }
+
+    public function setQuantidade($quantidade) {
+        $this->quantidade = $quantidade;
+    }
+
+    public function getDataCotacao() {
+        return $this->dataCotacao;
+    }
+
+    public function setDataCotacao($dataCotacao) {
+        $this->dataCotacao = $dataCotacao;
+    }
+}
+?>
