32cc45570f0260c86a17ed44a762c3697ecf151e
JP1005YT <godlolpro32@gmail.com>
2024-09-27 16:05:07 -0300

    update produto

--- assets/view/examples/produto.php
+++ assets/view/examples/produto.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../controller/produtoController.php';
+require_once __DIR__ . '/../../controller/produtoController.php';
 $controlador = new ControladorProdutos();
 ?>
 <!DOCTYPE html>
